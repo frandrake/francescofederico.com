81904e3187c1b2748def4717b32672def1be7b2e
Francesco Federico <133645320+frandrake@users.noreply.github.com>
2025-11-10 14:17:38 +0000

    Update SITE_TITLE and SITE_DESCRIPTION constants

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,4 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Francesco Federico | Chief Marketing Officer at S&P Global | Author of The Agentic CMO and Chronicles of Change | Non Executive Director';
+export const SITE_DESCRIPTION = 'Francesco Federico is a Global CMO with 17 years of experience at Fortune 200 companies. His early work on expert systems and artificial intelligence as a scholar and practitioner culminated with the publication of his business book "The Agentic CMO" and his regular newsletter "Chronicles of Change". In his daily practice, Francesco is evolving marketing organisation into a modern AI and data driven function bonding brand to demand across markets and geographies.';
